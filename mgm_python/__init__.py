bfd3c8eee66dab9c5b62112327256e9675c52d66
Sebastian Stricker <sebastian.stricker@iwr.uni-heidelberg.de>
2024-08-28 16:05:36 +0200

    Hide logger from public interface

--- mgm_python/__init__.py
+++ mgm_python/__init__.py
@@ -3,8 +3,8 @@ from ._pylibmgm import _register_api_logger
 from . import solver, io
 import logging
 
-LOGGER = logging.getLogger("libmgm")
-LOGGER.setLevel(logging.INFO)
+_LOGGER = logging.getLogger("libmgm")
+_LOGGER.setLevel(logging.INFO)
 
-_register_api_logger(LOGGER)
-io._register_io_logger(LOGGER)
+_register_api_logger(_LOGGER)
+io._register_io_logger(_LOGGER)
